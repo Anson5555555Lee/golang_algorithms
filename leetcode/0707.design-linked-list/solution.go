ac7c53d5cb610f63e4bb906e48e6b451258eefb9
Anson5555555Lee <frank5555555li@gmail.com>
2019-12-30 14:25:09 +0800

    add leetcode problem-0707

--- leetcode/0707.design-linked-list/solution.go
+++ leetcode/0707.design-linked-list/solution.go
@@ -1,1 +1,141 @@
 package problem0707
+
+type MyLinkedList struct {
+	Head, Tail *Node
+	Size       int
+}
+
+type Node struct {
+	Value int
+	Next  *Node
+}
+
+/** Initialize your data structure here. */
+func Constructor() MyLinkedList {
+	return MyLinkedList{nil, nil, 0}
+}
+
+/** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
+func (this *MyLinkedList) Get(index int) int {
+	if this.Head == nil || index >= this.Size {
+		return -1
+	}
+
+	curr := this.Head
+	for i := 0; i < index; i++ {
+		curr = curr.Next
+	}
+
+	return curr.Value
+}
+
+/** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
+func (this *MyLinkedList) AddAtHead(val int) {
+	newNode := &Node{val, nil}
+
+	if this.Head == nil {
+		this.Head = newNode
+		this.Tail = newNode
+	} else {
+		newNode.Next = this.Head
+		this.Head = newNode
+	}
+
+	this.Size++
+}
+
+/** Append a node of value val to the last element of the linked list. */
+func (this *MyLinkedList) AddAtTail(val int) {
+	newNode := &Node{val, nil}
+
+	if this.Tail == nil {
+		this.Head = newNode
+		this.Tail = newNode
+	} else {
+		this.Tail.Next = newNode
+		this.Tail = newNode
+	}
+
+	this.Size++
+}
+
+/** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
+func (this *MyLinkedList) AddAtIndex(index int, val int) {
+	if index == 0 {
+		this.AddAtHead(val)
+	} else if index == this.Size {
+		this.AddAtTail(val)
+	} else if index > this.Size {
+		return
+	} else {
+		curr := this.Head
+		count := 0
+		var prev *Node
+		for curr != nil {
+			if count == index {
+				newNode := &Node{val, nil}
+				newNode.Next = curr
+				prev.Next = newNode
+				break
+			}
+
+			prev = curr
+			curr = curr.Next
+			count++
+		}
+
+		this.Size++
+	}
+}
+
+/** Delete the index-th node in the linked list, if the index is valid. */
+func (this *MyLinkedList) DeleteAtIndex(index int) {
+	if index > this.Size {
+		return
+	}
+
+	var prev *Node
+	curr := this.Head
+	count := 0
+
+	for curr != nil {
+		if count == index {
+			// delete at head
+			if prev == nil {
+				if this.Head == this.Tail {
+					this.Head = nil
+					this.Tail = nil
+					this.Size = 0
+					return
+				}
+
+				this.Head = curr.Next
+				this.Size--
+				return
+			} else if index == this.Size-1 {
+				this.Tail = prev
+				this.Size--
+				return
+			} else {
+				prev.Next = curr.Next
+				this.Size--
+			}
+
+			break
+		}
+
+		prev = curr
+		curr = curr.Next
+		count++
+	}
+}
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * obj := Constructor();
+ * param_1 := obj.Get(index);
+ * obj.AddAtHead(val);
+ * obj.AddAtTail(val);
+ * obj.AddAtIndex(index,val);
+ * obj.DeleteAtIndex(index);
+ */
--- leetcode/0707.design-linked-list/solution_test.go
+++ leetcode/0707.design-linked-list/solution_test.go
@@ -1,1 +1,41 @@
 package problem0707
+
+import "testing"
+
+// ["MyLinkedList","addAtHead","addAtTail","addAtIndex","get","deleteAtIndex","get"]
+// [[],[1],[3],[1,2],[1],[1],[1]]
+
+func TestMyLinkedList(t *testing.T) {
+	list := Constructor()
+	list.AddAtHead(1)
+	if list.Head.Value != 1 ||
+		list.Head.Next != nil ||
+		list.Tail.Value != 1 ||
+		list.Tail.Next != nil ||
+		list.Size != 1 {
+		t.Fatal("wrong")
+	}
+	list.AddAtTail(3)
+	if list.Head.Value != 1 ||
+		list.Head.Next != list.Tail ||
+		list.Tail.Value != 3 ||
+		list.Tail.Next != nil ||
+		list.Size != 2 {
+		t.Fatal("wrong")
+	}
+	list.AddAtIndex(1, 2)
+	if list.Head.Value != 1 || list.Tail.Value != 3 || list.Head.Next.Value != 2 || list.Size != 3 {
+		t.Fatal("wrong")
+	}
+
+	val := list.Get(1)
+	if val != 2 {
+		t.Fatal("wrong")
+	}
+
+	list.DeleteAtIndex(1)
+	val = list.Get(1)
+	if val == 2 {
+		t.Fatal("wrong")
+	}
+}
