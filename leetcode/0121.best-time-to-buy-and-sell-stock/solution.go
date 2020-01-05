4f45055ffa3c2de23482610f7a951aa16b0d0c94
Anson5555555Lee <frank5555555li@gmail.com>
2020-01-05 09:02:41 +0800

    improve solution to problem 0121

--- leetcode/0121.best-time-to-buy-and-sell-stock/solution.go
+++ leetcode/0121.best-time-to-buy-and-sell-stock/solution.go
@@ -1,13 +1,43 @@
 package problem0121
 
+// Definition:
+// max_profit = max{price[j] - price[i]}, 0 <= i < j <= n-1
+
+// Solution 1: Brute force
+// Enumerate all pairs of (i, j)
+// Time complexity: O(n^2)
+
+// Solution 2: DP
+// Keep tracking the min price so far
+
+// Solution 3: DP
+
 func maxProfit(prices []int) int {
 	if len(prices) < 2 {
 		return 0
 	}
-	maxPrice, maxProfit := 0, 0
+	// we can also use array to trace the minPrice and maxProfit to show this is a dynamic programming
+	minPrice, maxProfit := prices[0], 0
 	for i := 1; i < len(prices); i++ {
-
+		minPrice = min(minPrice, prices[i-1])
+		maxProfit = max(maxProfit, prices[i]-minPrice)
 	}
 
 	return maxProfit
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+
+	return b
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+
+	return b
 }
--- leetcode/0121.best-time-to-buy-and-sell-stock/solution_test.go
+++ leetcode/0121.best-time-to-buy-and-sell-stock/solution_test.go
@@ -1,7 +1,62 @@
 package problem0121
 
-import "testing"
+import (
+	"fmt"
+	"testing"
 
-func TestSolution(t *testing.T) {
+	"github.com/stretchr/testify/assert"
+)
 
+type question struct {
+	para
+	ans
+}
+
+type para struct {
+	prices []int
+}
+
+type ans struct {
+	one int
+}
+
+func Test_Problem0121(t *testing.T) {
+	ast := assert.New(t)
+
+	qs := []question{
+
+		question{
+			para{
+				[]int{7, 2, 1, 5, 3, 6, 4},
+			},
+			ans{
+				5,
+			},
+		},
+
+		question{
+			para{
+				[]int{7, 1, 5, 3, 6, 4},
+			},
+			ans{
+				5,
+			},
+		},
+
+		question{
+			para{
+				[]int{7, 6, 5, 4, 3, 2, 1},
+			},
+			ans{
+				0,
+			},
+		},
+	}
+
+	for _, q := range qs {
+		a, p := q.ans, q.para
+		fmt.Printf("~~%v~~\n", p)
+
+		ast.Equal(a.one, maxProfit(p.prices), "input:%v", p)
+	}
 }
